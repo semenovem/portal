9199686ceb3e07f6dcbbc78810236b881624321e
Evg Semenov <semenovem@gmail.com>
2024-02-25 18:38:10 +0300

    wip

--- migrations/20230525141410_media.up.sql
+++ migrations/20230525141410_media.up.sql
@@ -24,19 +24,19 @@ CREATE TABLE IF NOT EXISTS media.upload_files
 -- предварительно наборы картинок
 CREATE TABLE IF NOT EXISTS media.upload_boxes
 (
-  id              serial PRIMARY KEY,
-  note            text default NULL,
-  upload_file_ids int[]              NOT NULL, -- список файлов, входящих в набор
-  deleted         bool default false NOT NULL
+  id              SERIAL PRIMARY KEY,
+  note            TEXT default NULL,
+  upload_file_ids INT[]              NOT NULL, -- список файлов, входящих в набор
+  deleted         BOOL default false NOT NULL
 );
 
 -- файлы
 CREATE TABLE IF NOT EXISTS media.files
 (
-  id          serial PRIMARY KEY,
-  hash_sha256 varchar            NOT NULL,
-  note        text default NULL,
-  deleted     bool default false NOT NULL,
+  id          SERIAL PRIMARY KEY,
+  hash_sha256 VARCHAR            NOT NULL,
+  note        TEXT DEFAULT NULL,
+  deleted     BOOL DEFAULT FALSE NOT NULL,
   kind        media.file_kind    NOT NULL
 
 --  хранение в S3
@@ -45,21 +45,21 @@ CREATE TABLE IF NOT EXISTS media.files
 -- наборы картинок
 CREATE TABLE IF NOT EXISTS media.boxes
 (
-  id       serial PRIMARY KEY,
-  note     text default NULL,
-  file_ids int[]              NOT NULL, -- список файлов, входящих в набор
-  deleted  bool default false NOT NULL
+  id       SERIAL PRIMARY KEY,
+  note     TEXT DEFAULT NULL,
+  file_ids INT[]              NOT NULL, -- список файлов, входящих в набор
+  deleted  BOOL DEFAULT FALSE NOT NULL
 );
 
 -- набор фото автомобиля
 CREATE TABLE IF NOT EXISTS media.vehicle_images
 (
-  id                    serial PRIMARY KEY,
-  cabin_image_box_id    int REFERENCES media.boxes, -- список картинок, входящих в набор
-  front_image_box_id    int REFERENCES media.boxes,
-  left_image_box_id     int REFERENCES media.boxes,
-  right_image_box_id    int REFERENCES media.boxes,
-  back_image_box_id     int REFERENCES media.boxes,
-  odometer_image_box_id int REFERENCES media.boxes,
-  other_image_box_id    int REFERENCES media.boxes
+  id                    SERIAL PRIMARY KEY,
+  cabin_image_box_id    INT REFERENCES media.boxes, -- список картинок, входящих в набор
+  front_image_box_id    INT REFERENCES media.boxes,
+  left_image_box_id     INT REFERENCES media.boxes,
+  right_image_box_id    INT REFERENCES media.boxes,
+  back_image_box_id     INT REFERENCES media.boxes,
+  odometer_image_box_id INT REFERENCES media.boxes,
+  other_image_box_id    INT REFERENCES media.boxes
 );
